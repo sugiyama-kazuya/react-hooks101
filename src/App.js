ce75323da6ccc74d8f108b25a09fc77b98cf80aa
Kazuya Sugiyama <kakao.0451a@gmail.com>
2021-01-03 18:56:11 +0900

    react hooks useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>This is a Temlate</div>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((previousCount) => ++previousCount);
+  };
+
+  const reset = () => setCount(0);
+
+  const third = () => {
+    setCount((pre) => (pre % 3 === 0 ? pre / 3 : pre));
+  };
+
+  return (
+    <>
+      <div>{count}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={reset}>reset</button>
+      <button onClick={third}>三の倍数</button>
+    </>
+  );
 };
 
 export default App;
