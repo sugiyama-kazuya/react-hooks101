084935101b64f74bf3969d74cf176b12a3c00219
Kazuya Sugiyama <kakao.0451a@gmail.com>
2021-01-05 11:40:05 +0900

    play useContext

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,19 @@ import React, { useReducer } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Events from "../components/Events";
 import EventForm from "../components/EventForm";
+import AppContext from "../contexts/AppContext";
 import reducer from "../reducers";
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch} />
-      <Events state={state} dispatch={dispatch} />
-    </div>
+    <AppContext.Provider value={"元気ですか？"}>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />
+      </div>
+    </AppContext.Provider>
   );
 };
 
